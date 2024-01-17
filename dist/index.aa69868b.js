aab5b58635984a0e8ecfe6ee00776487ba06b5aa
Özgur Celik <ozce2300@student.miun.se>
2024-01-17 23:21:31 +0100

    Lagt upp och optimerat bilder automatisk

---
+++ dist/index.aa69868b.js
@@ -0,0 +1,8 @@
+"use strict";
+setInterval(tiden, 100);
+function tiden() {
+    let date = new Date();
+    document.getElementById("klockan").innerHTML = date.toLocaleTimeString();
+}
+
+//# sourceMappingURL=index.aa69868b.js.map
